d112ce7d4d148cc43b0d2f05924a05dde3b23eda
deeksha1605 <deekshaonethird@gmail.com>
2017-03-24 19:34:00 +0900

    more chnages

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 var express = require('express');
-var fs = require("fs");
 var app = express();
-var async = require('async');
 
 app.use(express.static(__dirname + "/public")); //use static files in ROOT/public folder
 app.set('port', (process.env.PORT || 3000));
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
 
 app.listen(app.get('port'), function () {
   console.log('running on port', app.get('port'))
